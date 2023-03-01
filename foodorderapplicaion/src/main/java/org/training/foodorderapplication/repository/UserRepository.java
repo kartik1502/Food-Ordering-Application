826d5d6ecd11a88e62820cb1b869c5393a569c8f
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2023-03-01 23:04:47 +0530

    Delete foodorderapplicaion directory

--- foodorderapplicaion/src/main/java/org/training/foodorderapplication/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training.foodorderapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training.foodorderapplication.entity.Users;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
