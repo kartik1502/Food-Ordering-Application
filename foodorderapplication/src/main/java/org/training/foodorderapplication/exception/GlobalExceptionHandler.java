44b4d39c3f827a5c05bc3604945c5b0d41ec3f8a
DIVYASHREE S K <65600738+divya5140@users.noreply.github.com>
2023-03-02 09:49:52 +0530

    Update GlobalExceptionHandler.java

--- foodorderapplication/src/main/java/org/training/foodorderapplication/exception/GlobalExceptionHandler.java
+++ foodorderapplication/src/main/java/org/training/foodorderapplication/exception/GlobalExceptionHandler.java
@@ -18,17 +18,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		List<String> errorDetails = new ArrayList<>();
-		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-			errorDetails.add(error.getDefaultMessage());
-		}
-		ErrorResponse response = new ErrorResponse(400l, errorDetails);
-		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-	}
-
 	@ExceptionHandler(value = NoSearchDataException.class)
 	public ResponseEntity<Object> exception(NoSearchDataException exception) {
 		return new ResponseEntity<>("No such Food or Vendor exists", HttpStatus.NOT_FOUND);
